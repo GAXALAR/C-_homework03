cdf762636eeaa77c08c75c8a344a2599255cd9e9
GAXALAR <ay.kosyrev@gmail.com>
2023-01-25 18:23:44 +0300

    прошу прощения за сумбур, на работе завал(

--- task_01/Program.cs
+++ task_01/Program.cs
@@ -2,7 +2,7 @@
 string number = Console.ReadLine();
 
 void CheckingNumber(string number){
-  if (number[0]==number[4] || number[1]==number[3]){
+  if (number[0]==number[4] && number[1]==number[3]){
     Console.WriteLine($"Ваше число: {number} - палиндром.");
   }
   else Console.WriteLine($"Ваше число: {number} - НЕ палиндром.");
@@ -11,4 +11,6 @@ void CheckingNumber(string number){
 if (number.Length == 5){
   CheckingNumber(number);
 }
-else Console.WriteLine($"Введи правильное число");
+else {
+  Console.WriteLine($"Введи правильное число");
+}
